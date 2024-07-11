98d6ca18205af3cc3436ecbc2bf010e0d09761e2
unknown <cshravansingh45@gmail.com>
2024-07-11 23:47:09 +0530

    add tricky question

---
+++ Trickyquestion.js
@@ -0,0 +1,23 @@
+// console.log(a);
+// let a=1;
+
+// output is the canot access 'a' before initialization, dueto hosting reason 
+
+
+// console.log('1');
+// setTimeout(()=>
+// {
+//    console.log('2');
+// },1000);
+
+// setTimeout(()=>
+// {
+//     console.log('3');
+// },0);
+
+// console.log('4');
+
+// output is : 1432
+
+// console.log(1+2+'3'+2+2); 
+// output is : 3322
